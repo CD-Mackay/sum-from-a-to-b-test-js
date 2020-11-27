e2db4e2c9b95ef70511f2a1808eb537d737d59b0
CD-Mackay <connor.mackay@gmail.com>
2020-11-27 18:20:02 +0000

    recursive function

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  if (fromN === toN) {
+    return fromN;
+  } else {
+    return fromN + sum(fromN + 1, toN);
+  }
 }
 
 module.exports = sum;
+
+// 3 , 7
+3, 4, 5, 6, 7
+25;
+
